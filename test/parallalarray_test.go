db493cce08edf1d940e360bc272a3787492700ec
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-29 15:14:31 +0800

    updated test case

--- test/parallalarray_test.go
+++ test/parallalarray_test.go
@@ -4,7 +4,12 @@ import (
 	. "github.com/aabbcc1241/goutils/lang"
 	"math/rand"
 	"testing"
+	"time"
 )
+
+/* 1 thread  : 10.002 s
+ * 8 threads :  2.002 s
+ */
 
 var c = 0
 
@@ -13,10 +18,11 @@ type for_s struct {
 
 func (p for_s) Apply(k int, v Empty, r *rand.Rand) {
 	c++
+	time.Sleep(1 * time.Second)
 	//t.Log(k)
 }
 func TestParallelArray(t *testing.T) {
-	arr := ParallelArray{Data: make([]Empty, 1048576), NThread: 8}
+	arr := ParallelArray{Data: make([]Empty, 10), NThread: 8}
 	t.Log("array len:", len(arr.Data))
 	//t.Log("start parallel for loop")
 	arr.For(for_s{}, false)
